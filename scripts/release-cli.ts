0f40851c841c17076c2d84df7660bc0682f0a507
Punit Arani <punitsai36@gmail.com>
2026-04-03 01:56:14 -0700

    Simplify CLI release entrypoints

--- scripts/release-cli.ts
+++
@@ -1,9 +0,0 @@
-import process from "node:process";
-import { main } from "./releases/publish";
-
-if (import.meta.main) {
-  void main(["--package", "cli", ...process.argv.slice(2)]).catch((error) => {
-    console.error(error instanceof Error ? error.message : String(error));
-    process.exit(1);
-  });
-}
